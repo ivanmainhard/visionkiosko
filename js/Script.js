baa791d04aaf3abc96cce5179210aac20a676628
ivanmainhard <ivanmainhardepet20@gmail.com>
2021-10-27 20:06:46 -0300

    cambio de barra y arreglo de mapeo

--- js/Script.js
+++ js/Script.js
@@ -1,1 +1,10 @@
 window.alert('¿quiere ingresar a la pagina?')
+function updatemenu() {
+    if (document.getElementById('responsive-menu').checked == true) {
+      document.getElementById('menu').style.borderBottomRightRadius = '0';
+      document.getElementById('menu').style.borderBottomLeftRadius = '0';
+    }else{
+      document.getElementById('menu').style.borderRadius = '25px';
+    }
+  }
+  
